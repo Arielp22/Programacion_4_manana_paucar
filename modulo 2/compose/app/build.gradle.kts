e8c164606c5176412741fd4a419d6919597c4d4e
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-05-07 11:00:47 -0500

    Clase 07-05-2026

--- modulo 2/compose/app/build.gradle.kts
+++ modulo 2/compose/app/build.gradle.kts
@@ -50,10 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.text)
     implementation(libs.androidx.constraintlayout.compose)
     implementation(libs.androidx.compose.material3.lint)
-    implementation(libs.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.compose.material:material-icons-extended")
 }
