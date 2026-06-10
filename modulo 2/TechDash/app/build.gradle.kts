d663398ea35d2ff8333f699825d39c05beff03d0
Arielp22 <earielp2000@hotmail.com>
2026-06-10 05:47:26 -0500

    Migracion rama segundo corte

--- modulo 2/TechDash/app/build.gradle.kts
+++ modulo 2/TechDash/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ute.techdash"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -53,6 +53,21 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    // Material Icons (necesario para Icons.Default.CameraAlt, Icons.Default.NoPhotography, etc.)
     implementation("androidx.compose.material:material-icons-extended")
+
+
+    val cameraxVersion = "1.4.1"
+    implementation("androidx.camera:camera-core:$cameraxVersion")
+    implementation("androidx.camera:camera-camera2:$cameraxVersion")
+    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
+    implementation("androidx.camera:camera-view:$cameraxVersion")
+    implementation("androidx.camera:camera-video:$cameraxVersion")
+
+    // Media3 / ExoPlayer para reproducción de video
+    val media3Version = "1.5.1"
+    implementation("androidx.media3:media3-exoplayer:$media3Version")
+    implementation("androidx.media3:media3-ui:$media3Version")
+
+    // Coil para mostrar imágenes
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
